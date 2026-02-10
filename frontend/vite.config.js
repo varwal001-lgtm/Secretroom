7c5284c59d0a8d4cc5ea92bab94f96b6d4dda6cf
Gurdeep Singh <varwal001@gmail.com>
2026-02-10 23:04:47 +0530

    deploy frontend build for github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,1 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/chatpe/",
-});
+export default { base: '/Secretroom/' } 
